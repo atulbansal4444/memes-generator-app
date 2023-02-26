047fc16acee7337c42dcea4eadac7ad3a7a5f7ec
Atul Bansal <atulbansal4444@gmail.com>
2023-02-27 02:47:43 +0530

    Changes: Added new fetch memes actions and reducers to handle state

---
+++ src/actions/fetch.js
@@ -0,0 +1,41 @@
+const REQUEST_MEMES = "REQUEST_MEMES";
+const requestMemes = () => {
+  return {
+    type: REQUEST_MEMES,
+  };
+};
+
+const REQUEST_MEMES_SUCCESS = "REQUEST_MEMES_SUCCESS";
+const requestMemesSuccess = (data) => {
+  return {
+    type: REQUEST_MEMES_SUCCESS,
+    payload: data,
+  };
+};
+
+const REQUEST_MEMES_ERROR = "REQUEST_MEMES_ERROR";
+const requestMemesError = (errMsg) => {
+  return {
+    type: REQUEST_MEMES_ERROR,
+    payload: errMsg,
+  };
+};
+
+const fetchMemes = () => (dispatch) => {
+  dispatch(requestMemes());
+  const url = "https://api.imgflip.com/get_memes";
+  fetch(url)
+    .then((response) => response.json())
+    .then(json => dispatch(requestMemesSuccess(json.data)))
+    .catch((err) => dispatch(requestMemesError(err)));
+};
+
+export {
+  REQUEST_MEMES,
+  requestMemes,
+  REQUEST_MEMES_SUCCESS,
+  requestMemesSuccess,
+  REQUEST_MEMES_ERROR,
+  requestMemesError,
+  fetchMemes,
+};
